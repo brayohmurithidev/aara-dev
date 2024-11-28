aa53bde8247a137d6e1935b84fd708a3a9a950ed
brayohmurithidev <murithibrianm@gmail.com>
2024-11-28 13:53:20 +0300

    test logout

--- app/(auth)/(tabs)/index.tsx
+++ app/(auth)/(tabs)/index.tsx
@@ -1,10 +1,17 @@
-import {Text} from "react-native-paper";
-
+import { Button, Text } from "react-native-paper";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useAuth } from "@/context/AuthProvider.tsx";
 
 const Home = () => {
-    return (
-        <Text>Home</Text>
-    )
-}
+  const { signOut } = useAuth();
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+      <Button mode="contained" onPress={signOut}>
+        Logout
+      </Button>
+    </SafeAreaView>
+  );
+};
 
-export default Home
+export default Home;
