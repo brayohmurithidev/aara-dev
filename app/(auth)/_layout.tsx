90e314ee319278b3823cc282256304d0b15b69dd
brayohmurithidev <murithibrianm@gmail.com>
2024-11-28 17:58:40 +0300

    wrap auth with chatprovider

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,11 +1,14 @@
-import {Stack} from "expo-router";
+import { Stack } from "expo-router";
+import ChatProvider from "@/context/ChatProvider.tsx";
 
-const AuthLayout = ()=> {
-    return(
-        <Stack>
-            <Stack.Screen name='(tabs)' />
-        </Stack>
-    )
-}
+const AuthLayout = () => {
+  return (
+    <ChatProvider>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </ChatProvider>
+  );
+};
 
 export default AuthLayout;
