4ccf336a1abc0d4cea3d0d7498fdd1474e6afe1d
brayohmurithidev <murithibrianm@gmail.com>
2024-11-28 13:51:30 +0300

    added google login component

---
+++ components/GoogleLogin.tsx
@@ -0,0 +1,11 @@
+import { GoogleSigninButton } from "@react-native-google-signin/google-signin";
+
+// GoogleOneTapSignIn.configure({
+//   webClientId: "autoDetect",
+// });
+
+const GoogleLogin = () => {
+  return <GoogleSigninButton />;
+};
+
+export default GoogleLogin;
